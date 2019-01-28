47761a08561db4c189f9d631609af3dc21359376
Justin Li <git@justinli.net>
2019-01-28 13:57:37 -0500

    Add -host flag

--- serve.go
+++ serve.go
@@ -11,11 +11,15 @@ import (
 func main() {
 	port := flag.Int("port", 8080, "Port to listen on")
 	path := flag.String("path", ".", "Directory to serve")
+	host := flag.String("host", "", "Host to listen on")
 	flag.Parse()
+
+	addr := *host + ":" + strconv.Itoa(*port)
 
 	fullPath, _ := filepath.Abs(*path)
 	fullPath = filepath.Clean(fullPath)
-	log.Println("Serving", fullPath, "on", *port)
 
-	panic(http.ListenAndServe(":"+strconv.Itoa(*port), http.FileServer(http.Dir(fullPath))))
+	log.Println("Serving", fullPath, "on", addr)
+
+	panic(http.ListenAndServe(addr, http.FileServer(http.Dir(fullPath))))
 }
